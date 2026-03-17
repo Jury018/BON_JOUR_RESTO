85fb177d79fbc8a41afaa68c68d76cf3222aa6b5
Jury018 <jurypecaoco5360@gmail.com>
2026-03-17 22:09:39 +0800

    feat: Add Firebase configuration and related generated files to the project.

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,8 +1,6 @@
-// Firebase Web SDK Configuration
-// This is used for Client-side Auth and Analytics
 const firebaseConfig = {
   apiKey: "AIzaSyB3WepVTRUMKtjBpdUGrB8XfMve-ZUCjWs",
-  authDomain: "bon-jour-base.firebaseapp.com",
+  authDomain: "bonjour.base69.studio",
   databaseURL: "https://bon-jour-base-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "bon-jour-base",
   storageBucket: "bon-jour-base.appspot.com",
